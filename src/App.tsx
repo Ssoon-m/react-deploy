e2d6577ae5ab40c48eccec7d1002476bc944afc2
순민 <tkksm2@gmail.com>
2022-11-12 16:48:43 +0900

    feat: aws cli 를 통해 s3에 배포하기

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>리액트 프로젝트 S3 + CloudFront 배포</p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
-          Learn React
+          SOOOOOON MIN
         </a>
       </header>
     </div>

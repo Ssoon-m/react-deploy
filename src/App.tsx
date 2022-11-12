319bb1689373bdb40465ca3b72b93a0c8b752ff6
순민 <tkksm2@gmail.com>
2022-11-12 19:58:00 +0900

    feat: cloudfront cash purge 기능

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          SOOOOOON MIN
+          SOOOOOON MIN[Invalidate 테스트]
         </a>
       </header>
     </div>
